729dc34bdc29dd335fe16a7c2a81165c565b6c3f
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2024-12-25 00:51:36 +0900

    sutdy-operator

---
+++ hanbit/chap02/operator03.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>연산자</title>
+</head>
+<body>
+    <?php
+    $a = 5;
+    $b = 10;
+
+    echo "a = ", $a, "<br>";
+    echo "b = ", $b, "<br>";
+
+    echo "a > b => ", ($a > $b), "<br>";
+    echo "!(a > b) => ",!($a > $b), "<br>";
+    echo "(a == b) && (a == 5) => ", (($a == $b) && ($a == 5)) , "<br>";
+    echo "(a != b) && (a == 5) => ", (($a != $b) && ($a == 5)), "<br>";
+    echo "!((a != b) && (a == 5)) => ", (!(($a != $b) && ($a == 5))), "<br>";
+
+    ?>
+</body>
+</html>
