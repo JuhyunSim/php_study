d8f474f0c79cbe60dd9be1fd2cee04d1cb5f76b3
Sim Juhyun <154858191+JuhyunSim@users.noreply.github.com>
2025-01-23 22:47:53 +0900

    study-database/connect&createTable

---
+++ hanbit/chap13/mysqli02.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>데이터베이스 연동</title>
+    </head>
+    <body>
+        <?php
+        $servername = 'localhost';
+        $username = 'admin';
+        $password = 'admin1234';
+        $dbname = 'phpbooddb';
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if ($conn) {
+            die('데이터베이스 연결 실패 : ' . mysqli_connect_error());
+        } 
+        echo '데이터베이스 연결 성공: ' .mysqli_get_host_info($conn);
+
+        $sql = "CREATE TABLE IF NOT EXISTS member(
+            id VARCHAR(20) NOT NULL,
+            passwd VARCHAR(20),
+            name VARCHAR(30),
+            PRIMARY KEY (id))";
+
+        if (mysqli_query($conn, $sql)) {
+            echo '테이블 생성 성공';
+        } else {
+            echo '테이블 생성 실패' . mysqli_error($conn);
+        }
+        mysqli_close();
+        ?>
+    </body>
+</html>
